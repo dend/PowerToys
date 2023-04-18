2f593a665943a528fea0703d887da6732212e92f
Den <1389609+dend@users.noreply.github.com>
2023-04-17 17:01:08 -0700

    Add struct

---
+++ src/modules/awake/Awake/Core/Models/NotifyIconData.cs
@@ -0,0 +1,39 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Runtime.InteropServices;
+
+namespace Awake.Core.Models
+{
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.NamingRules", "SA1307:Accessible fields should begin with upper-case letter", Justification = "Following Win32 API conventions.")]
+    internal struct NotifyIconData
+    {
+        public int cbSize = Marshal.SizeOf(typeof(NotifyIconData));
+        public IntPtr hWnd;
+        public int uID;
+        public int uFlags;
+        public int uCallbackMessage;
+        public IntPtr hIcon;
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 128)]
+        public string szTip;
+        public int dwState;
+        public int dwStateMask;
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 256)]
+        public string szInfo;
+        public int uTimeoutOrVersion;
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 64)]
+        public string szInfoTitle;
+        public int dwInfoFlags;
+        public Guid guidItem;
+        public IntPtr hBalloonIcon;
+
+        public NotifyIconData()
+        {
+            szTip = string.Empty;
+            szInfoTitle = string.Empty;
+            szInfo = string.Empty;
+        }
+    }
+}
